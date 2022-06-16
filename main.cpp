94a24dc11deafe204422911db2d17504c452c9bc
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-15 23:27:19 -0700

    Add create database

--- main.cpp
+++ main.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
 #include <sqlite3.h>
 
-int main(int argc, char** argv) {
-	sqlite3* DB;
-	int exit;
+const std::string create_users_str =
+    "CREATE TABLE USER("
+    "UserId INT PRIMARY KEY	NOT NULL, "
+    "Name TEXT				NOT NULL, "
+    "Username TEXT			NOT NULL, "
+    "Email TEXT				NOT NULL);";
 
-	exit = sqlite3_open("example.db", &DB);
+const std::string create_tasks_str =
+    "CREATE TABLE TASKS("
+    "TaskId INT PRIMARY KEY	NOT NULL, "
+    "Message TEXT		NOT NULL, "
+	"UserId	INT			NOT NULL, "
+    "FOREIGN KEY(UserId) REFERENCES USER(UserId));";
 
-	if (exit) {
-		std::cerr << "Error opening DB " << sqlite3_errmsg(DB) << '\n';
-		return -1;
-	} else {
-		std::cout << "Opened database successfully!" << '\n';
-	}
+void create_table(sqlite3 *DB, std::string create_str) {
+    int status;
+    char *message_error;
 
-	sqlite3_close(DB);
-	return 0;
+    status = sqlite3_exec(DB, create_str.c_str(), NULL, 0, &message_error);
+
+    if (status != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", message_error);
+        sqlite3_free(message_error);
+    } else {
+        std::cout << "Table created Successfully" << '\n';
+    }
+}
+
+int main(int argc, char **argv) {
+    sqlite3 *DB;
+    int exit;
+    int status;
+
+    exit = sqlite3_open("example.db", &DB);
+
+    if (exit) {
+        std::cerr << "Error opening DB " << sqlite3_errmsg(DB) << '\n';
+        return -1;
+    } else {
+        std::cout << "Opened database successfully!" << '\n';
+    }
+
+    create_table(DB, create_users_str);
+    create_table(DB, create_tasks_str);
+
+    sqlite3_close(DB);
+    return 0;
 }
